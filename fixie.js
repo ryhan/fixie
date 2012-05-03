72e655f58bdc648ed7bb6891c2b6c9c019bc187f
Ilter Canberk <ilteriscanberk@gmail.com>
2012-05-03 02:13:49 -0400

    select query implemented

--- fixie.js
+++ fixie.js
@@ -8,27 +8,26 @@
  * Automagically adds filler content
  * whenever an element has class="fixie".
  * Hope you find it useful :)
- */ 
-(
-function () {
+ */
+var fixie = {
 
-    function getElementsByAttribute(oElm, strTagName, strAttributeName, strAttributeValue){
-    var arrElements = (strTagName == "*" && oElm.all)? oElm.all : oElm.getElementsByTagName(strTagName);
-    var arrReturnElements = new Array();
-    var oAttributeValue = (typeof strAttributeValue != "undefined")? new RegExp("(^|\\s)" + strAttributeValue + "(\\s|$)", "i") : null;
-    var oCurrent;
-    var oAttribute;
-    for(var i=0; i<arrElements.length; i++){
-        oCurrent = arrElements[i];
-        oAttribute = oCurrent.getAttribute && oCurrent.getAttribute(strAttributeName);
-        if(typeof oAttribute == "string" && oAttribute.length > 0){
-            if(typeof strAttributeValue == "undefined" || (oAttributeValue && oAttributeValue.test(oAttribute))){
-                arrReturnElements.push(oCurrent);
+    getElementsByAttribute: function (oElm, strTagName, strAttributeName, strAttributeValue) {
+        var arrElements = (strTagName == "*" && oElm.all) ? oElm.all : oElm.getElementsByTagName(strTagName);
+        var arrReturnElements = new Array();
+        var oAttributeValue = (typeof strAttributeValue != "undefined") ? new RegExp("(^|\\s)" + strAttributeValue + "(\\s|$)", "i") : null;
+        var oCurrent;
+        var oAttribute;
+        for (var i = 0; i < arrElements.length; i++) {
+            oCurrent = arrElements[i];
+            oAttribute = oCurrent.getAttribute && oCurrent.getAttribute(strAttributeName);
+            if (typeof oAttribute == "string" && oAttribute.length > 0) {
+                if (typeof strAttributeValue == "undefined" || (oAttributeValue && oAttributeValue.test(oAttribute))) {
+                    arrReturnElements.push(oCurrent);
+                }
             }
         }
-    }
-    return arrReturnElements;
-}
+        return arrReturnElements;
+    },
     /* 
      * Spec
      * Here are some functions you might find useful
@@ -47,7 +46,7 @@ function () {
      * Takes in an element and adds filler content.
      * Returns false if tag is unrecognized.
      */
-    function fixie_handler(element) {
+    handler: function (element) {
         switch (element.nodeName.toLowerCase()) {
         case 'b':
         case 'em':
@@ -79,7 +78,7 @@ function () {
         case 'h4':
         case 'h5':
         case 'h6':
-            element.innerHTML = fixie_fetchPhrase();
+            element.innerHTML = this.fetchPhrase();
             break;
 
         case 'footer':
@@ -87,19 +86,19 @@ function () {
         case 'summary':
         case 'blockquote':
         case 'p':
-            element.innerHTML = fixie_fetchParagraph();
+            element.innerHTML = this.fetchParagraph();
             break;
 
         case 'article':
         case 'section':
 
-            element.innerHTML = fixie_fetchParagraphs()
+            element.innerHTML = this.fetchParagraphs()
             break;
 
             /* Special cases */
         case 'a':
             element.href = "http://ryhan.me/";
-            element.innerHTML = "www." + fixie_fetchWord() + fixie_capitalize(fixie_fetchWord()) + ".com";
+            element.innerHTML = "www." + this.fetchWord() + this.capitalize(this.fetchWord()) + ".com";
             break;
 
         case 'img':
@@ -117,66 +116,68 @@ function () {
             break;
 
         default:
-            element.innerHTML = fixie_fetchSentence();
+            element.innerHTML = this.fetchSentence();
             return false;
         }
         return true;
-    }
+    },
+    wordlibrary: ["I", "8-bit", "ethical", "reprehenderit", "delectus", "non", "latte", "fixie", "mollit", "authentic", "1982", "moon", "helvetica", "dreamcatcher", "esse", "vinyl", "nulla", "Carles", "bushwick", "bronson", "clothesline", "fin", "frado", "jug", "kale", "organic", "local", "fresh", "tassel", "liberal", "art", "the", "of", "bennie", "chowder", "daisy", "gluten", "hog", "capitalism", "is", "vegan", "ut", "farm-to-table", "etsy", "incididunt", "sunt", "twee", "yr", "before", "gentrify", "whatever", "wes", "Anderson", "chillwave", "dubstep", "sriracha", "voluptate", "pour-over", "esse", "trust-fund", "Pinterest", "Instagram", "DSLR", "vintage", "dumpster", "totally", "selvage", "gluten-free", "brooklyn", "placeat", "delectus", "sint", "magna", "brony", "pony", "party", "beer", "shot", "narwhal", "salvia", "letterpress", "art", "party", "street-art", "seitan", "anime", "wayfarers", "non-ethical", "viral", "iphone", "anim", "polaroid", "gastropub", "city", 'classy', 'original', 'brew'],
 
-
-    var fixie_wordlibrary = ["I", "8-bit", "ethical", "reprehenderit", "delectus", "non", "latte", "fixie", "mollit", "authentic", "1982", "moon", "helvetica", "dreamcatcher", "esse", "vinyl", "nulla", "Carles", "bushwick", "bronson", "clothesline", "fin", "frado", "jug", "kale", "organic", "local", "fresh", "tassel", "liberal", "art", "the", "of", "bennie", "chowder", "daisy", "gluten", "hog", "capitalism", "is", "vegan", "ut", "farm-to-table", "etsy", "incididunt", "sunt", "twee", "yr", "before", "gentrify", "whatever", "wes", "Anderson", "chillwave", "dubstep", "sriracha", "voluptate", "pour-over", "esse", "trust-fund", "Pinterest", "Instagram", "DSLR", "vintage", "dumpster", "totally", "selvage", "gluten-free", "brooklyn", "placeat", "delectus", "sint", "magna", "brony", "pony", "party", "beer", "shot", "narwhal", "salvia", "letterpress", "art", "party", "street-art", "seitan", "anime", "wayfarers", "non-ethical", "viral", "iphone", "anim", "polaroid", "gastropub", "city", 'classy', 'original', 'brew']
-
-    function fixie_capitalize(string) {
+    capitalize: function (string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
-    }
+    },
 
-    function fixie_fetchWord() {
-        return fixie_wordlibrary[Math.round(Math.random() * (fixie_wordlibrary.length - 1))];
-    }
+    fetchWord: function () {
+        return this.wordlibrary[Math.round(Math.random() * (this.wordlibrary.length - 1))];
+    },
 
-    function fixie_fetchPhrase() {
+    fetchPhrase: function () {
         var fixie_length = Math.round(Math.random() * 2 + 3);
         var fixie_str = "";
         for (var fixie_i = 0; fixie_i < fixie_length - 1; fixie_i++) {
-            fixie_str += fixie_fetchWord() + " ";
+            fixie_str += this.fetchWord() + " ";
         }
-        fixie_str += fixie_fetchWord();
-        return fixie_capitalize(fixie_str);
-    }
+        fixie_str += this.fetchWord();
+        return this.capitalize(fixie_str);
+    },
 
-    function fixie_fetchSentence() {
+    fetchSentence: function () {
         var fixie_length = Math.round(Math.random() * 5 + 4);
         var fixie_str = "";
         for (var fixie_i = 0; fixie_i < fixie_length - 1; fixie_i++) {
-            fixie_str += fixie_fetchWord() + " ";
+            fixie_str += this.fetchWord() + " ";
         }
-        fixie_str += fixie_fetchWord() + ".";
-        return fixie_capitalize(fixie_str);
-    }
+        fixie_str += this.fetchWord() + ".";
+        return this.capitalize(fixie_str);
+    },
 
-    function fixie_fetchParagraph() {
+    fetchParagraph: function () {
         var fixie_length = Math.round(Math.random() * 4 + 3);
         var fixie_str = "";
         for (var fixie_i = 0; fixie_i < fixie_length - 1; fixie_i++) {
-            fixie_str += fixie_fetchSentence() + " ";
+            fixie_str += this.fetchSentence() + " ";
         }
-        fixie_str += fixie_fetchSentence();
-        return fixie_capitalize(fixie_str);
-    }
+        fixie_str += this.fetchSentence();
+        return this.capitalize(fixie_str);
+    },
 
-    function fixie_fetchParagraphs() {
+    fetchParagraphs: function () {
         var fixie_length = Math.round(Math.random() * 4 + 3);
         var fixie_str = "";
         for (var fixie_i = 0; fixie_i < fixie_length - 1; fixie_i++) {
-            fixie_str += "<p>" + fixie_fetchParagraph() + "</p>";
+            fixie_str += "<p>" + this.fetchParagraph() + "</p>";
         }
         return fixie_str;
+    },
+    attribute: "data-fixie",
+    init: function (str) {
+        var to_be_fixied = this.getElementsByAttribute(document, "*", this.attribute, "true");
+        for (var fixie_i = 0; fixie_i < to_be_fixied.length; fixie_i++) {
+            fixie.handler(to_be_fixied[fixie_i]);
+        }
+        var elem = document.querySelectorAll(str);
+        for (var fixie_i = 0; fixie_i < elem.length; fixie_i++) {
+            this.handler(elem[fixie_i]);
+        }
     }
-
-    var attribute = "data-fixie";
-    var to_be_fixied = getElementsByAttribute(document, "*", attribute, "true");
-    for (var fixie_i = 0; fixie_i < to_be_fixied.length; fixie_i++) {
-       fixie_handler(to_be_fixied[fixie_i]);
-    }
-
-})();
+};
