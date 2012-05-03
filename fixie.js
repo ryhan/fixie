3e501dd53c622a4d9d49d9aac50bdf3133a3b2fb
Ryhan Hassan <rthprog@gmail.com>
2012-05-03 00:00:04 -0400

    Supports browsers that don't have getElementsByClassName

--- fixie.js
+++ fixie.js
@@ -10,18 +10,18 @@
 (
 
 function () {
-    if (typeof window.myFunction != 'function') {
-        document.getElementsByClassName = function (cl) {
-            var retnode = [];
-            var myclass = new RegExp('\\b' + cl + '\\b');
-            var elem = this.getElementsByTagName('*');
-            for (var i = 0; i < elem.length; i++) {
-                var classes = elem[i].className;
-                if (myclass.test(classes)) retnode.push(elem[i]);
-            }
-            return retnode;
-        };
-    }
+if (typeof window.myFunction != 'function') {
+    document.getElementsByClassName = function (cl) {
+        var retnode = [];
+        var myclass = new RegExp('\\b' + cl + '\\b');
+        var elem = this.getElementsByTagName('*');
+        for (var i = 0; i < elem.length; i++) {
+            var classes = elem[i].className;
+            if (myclass.test(classes)) retnode.push(elem[i]);
+        }
+        return retnode;
+    };
+}
     /* 
      * Spec
      * Here are some functions you might find useful
